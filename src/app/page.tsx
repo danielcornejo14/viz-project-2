0d5ed0c33aa610029558ddfa7b43810e38669895
danicor1401 <danicor1401@gmail.com>
2024-05-24 04:27:49 +0000

    feat: added radial tree node interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import TreeInBunches from "./components/TreeInBunches";
+import { promises as fs } from 'fs'
+import ForceSimulationTree from './components/TreeInBunches';
 
-export default function Home() {
+
+export default async function Home() {
+
+  const cwd = process.cwd();
+
+  const costaRicaDataFile = await fs.readFile(cwd+'/public/data/flareHierarchy.json', 'utf8');
+  const costaRicaData = JSON.parse(costaRicaDataFile);
+
+
   return (
-   <TreeInBunches name="Test" width={100} height={100}></TreeInBunches>
+   <ForceSimulationTree width={950} height={950} data={costaRicaData}></ForceSimulationTree>
   );
 }
